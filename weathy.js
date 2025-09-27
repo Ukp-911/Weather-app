81d4da59e054936adf012c6ade39d9da37e11f70
Ukp-911 <codingukp@gmail.com>
2025-09-28 00:03:51 +0530

    some changes in input functioning

--- weathy.js
+++ weathy.js
@@ -3,29 +3,47 @@ let searchbtn = document.querySelector("#search");
 let city_err = document.querySelector(".city-error");
 let search_city = document.querySelector(".search-city");
 let weather = document.querySelector(".weather-climate-details")
-search.addEventListener("click", () => {
-    if (locationbox.value == "") {
+
+let apikey = "937496b707d135148466ca0f29cad6f9";
+
+const display_onscreen = (value) => {
+    if (value === "false") {
         city_err.style.display = "";
         weather.style.display = "none";
+        search_city.style.display = "none";
     } else {
-        console.log(locationbox.value);
-        locationbox.value = "";
         city_err.style.display = "none";
         weather.style.display = "";
+        search_city.style.display = "none";
     }
-    search_city.style.display = "none";
+}
+
+const weather_details = async (place) => {
+    if (place == "") {
+        display_onscreen("false");
+    } else {
+        let weather_data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apikey}`);
+        let clear_data = await weather_data.json();
+        if (clear_data.cod == "404") {
+            display_onscreen("false");
+        } else if (clear_data.cod == "200") {
+            display_onscreen("true");
+            let temp = ((clear_data.main.temp) - 32) * 5 / 9;
+            temp = Math.floor(temp * 10) / 10;
+            console.log(temp);
+        } else {
+            display_onscreen("false");
+        }
+    }
+
+}
+
+search.addEventListener("click", () => {
+    weather_details(locationbox.value);
 })
+
 locationbox.addEventListener("keydown", (evt) => {
     if (evt.key == "Enter") {
-        if (locationbox.value == "") {
-            city_err.style.display = "";
-            weather.style.display = "none";
-        } else {
-            console.log(locationbox.value);
-            locationbox.value = "";
-            city_err.style.display = "none";
-            weather.style.display = "";
-        }
-        search_city.style.display = "none";
+        weather_details(locationbox.value);
     }
 })
