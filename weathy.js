063e13425520bf3899000511609f7daf9023738a
Ukp-911 <codingukp@gmail.com>
2025-09-28 22:27:34 +0530

    worked on displaying of data

--- weathy.js
+++ weathy.js
@@ -18,6 +18,39 @@ const display_onscreen = (value) => {
     }
 }
 
+const display_temperature = (clear_data) => {
+    let temp = clear_data.main.temp - 273.15;
+    temp = Math.floor(temp * 10) / 10;
+    let tempbox = document.querySelector(".temperature");
+    tempbox = tempbox.firstElementChild;
+    tempbox.innerText = `${temp} °C`;
+}
+
+const display_windspeed=(clear_data)=>{
+    let windbox=document.querySelector(".wind-condition").querySelector(":nth-child(2)");
+    windbox.innerText=`${clear_data.wind.speed} m/s`;
+}
+
+const display_place=(clear_data)=>{
+    let placebox=document.querySelector(".place").querySelector(":nth-child(2)");
+    placebox.innerText=`${clear_data.name}`
+}
+
+const display_humid=(clear_data)=>{
+    let humidbox=document.querySelector(".climate-condition").querySelector(":nth-child(2)");
+    humidbox.innerText=`${clear_data.main.humidity}%`
+}
+
+const display_date=(clear_data)=>{
+    let datebox=document.querySelector(".date");
+    let date = clear_data.dt;
+    date = new Date(date * 1000);
+    let month = date.getMonth() + 1;      
+    let day = date.getDate();
+    let year = date.getFullYear();
+    datebox.innerText=`${month}/${day}/${year}`;
+}
+
 const weather_details = async (place) => {
     if (place == "") {
         display_onscreen("false");
@@ -28,14 +61,11 @@ const weather_details = async (place) => {
             display_onscreen("false");
         } else if (clear_data.cod == "200") {
             display_onscreen("true");
-            let temp = (clear_data.main.temp - 32) * 5 / 9;
-            temp = Math.floor(temp * 10) / 10;
-            console.log(temp);
-            let tempbox=document.querySelector(".temperature");
-            tempbox=tempbox.firstElementChild;
-            tempbox.innerText=`${temp} °C`;
-
-
+            display_temperature(clear_data);
+            display_windspeed(clear_data);
+            display_place(clear_data);
+            display_humid(clear_data);
+            display_date(clear_data);
         } else {
             display_onscreen("false");
         }
